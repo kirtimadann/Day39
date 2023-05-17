18b518a42213481df1f8dd87b42716910b6d145e
lunainfotec <kirtimadan1997@gmail.com>
2023-05-17 09:49:18 +0530

    Added : UC1

---
+++ EmployeeAttendenceUc1.js
@@ -0,0 +1,10 @@
+
+let isFullTime = 1;
+let empcheckrandom = Math.floor(Math.random()*2);
+
+console.log(empcheckrandom);
+if(empcheckrandom == isFullTime){
+    console.log("is Present");
+}else{
+    console.log("not present");
+}
